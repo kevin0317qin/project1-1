fd0ba68e022c4603293ec523f86548f4096cd32b
Olga Karpenko <okarpenko@usfca.edu>
2019-08-28 01:54:45 -0700

    Starter code and input files for project 1.

---
+++ src/main/java/hotelapp/HotelSearch.java
@@ -0,0 +1,24 @@
+package hotelapp;
+
+
+/** The main class for project 1.
+ * The main function should take the following 4 command line arguments:
+ * -hotels hotelFile -reviews reviewsDirectory
+ *
+ * and read general information about the hotels from the hotelFile (a JSON file)
+ * and read hotel reviews from the json files in reviewsDirectory.
+ * The data should be loaded into data structures that allow efficient search.
+ * See Readme for details.
+ *
+ * You are expected to add other classes and methods to this project.
+ */
+public class HotelSearch {
+    public static void main(String[] args) {
+       // FILL IN CODE:
+        // Note that you are expected to create additional classes and methods
+
+
+    }
+
+
+}
